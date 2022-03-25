1add60b2368cf3023f6071e1b34734c7e73418e0
mec312 <mec_312@hotmail.com>
2022-03-25 18:38:31 -0500

    Delete Accounts/bankaccount directory

--- Accounts/bankaccount/src/main/java/com/bootcamp/bankaccount/service/ClientService.java
+++
@@ -1,9 +0,0 @@
-package com.bootcamp.bankaccount.service;
-
-import com.bootcamp.bankaccount.models.dto.ClientQuery;
-import reactor.core.publisher.Mono;
-
-public interface ClientService {
-
-    Mono<ClientQuery> getClient(String clientIdNumber);
-}
